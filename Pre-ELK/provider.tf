118e81f048be9acc69f671c7dfe302c5d726cfd5
shiv-rajawat <bindassmangal@gmail.com>
2018-10-12 21:09:49 +0530

    Update provider.tf

--- Pre-ELK/provider.tf
+++ Pre-ELK/provider.tf
@@ -1,5 +1,5 @@
 ## This file containes provider's info. Secret key id and secret key can also be included inside provider block. For more info see:
 ## https://www.terraform.io/docs/providers/aws/#
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
