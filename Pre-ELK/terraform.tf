79708ba29edb4938e0ec27fd64e2a756140088ad
shiv-rajawat <bindassmangal@gmail.com>
2018-10-17 17:57:31 +0530

    Update terraform.tf

--- Pre-ELK/terraform.tf
+++ Pre-ELK/terraform.tf
@@ -4,6 +4,5 @@ terraform {
     bucket = "cwt-terraform-remote-state-bucket"
     dynamodb_table = "cwt-terraform-state-lock"
     key    = "global/s3/pre-elk-terraform.tfstate"
-    region = "us-east-2"
   }
 }
