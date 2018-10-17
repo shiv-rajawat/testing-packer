ea3a30f1e9d24e500cd244bc27f6c5e86e044efc
Shiv Rajawat <shivrajawat@tabahi.com>
2018-10-17 15:37:33 +0530

    Century

---
+++ pipeline-vars.groovy
@@ -0,0 +1,14 @@
+// the stages this pipeline supports
+env.PIPELINE_STEPS = [
+    Build: "./scripts/build.sh",
+    Validate: "./scripts/validate.sh",
+    Deploy: "./scripts/deploy.sh",
+];
+
+// Credential Tokens
+env.AWS_CREDENTIAL_ID_TOKEN = 'TBD'
+env.AWS_ACCESS_KEY_ID_TOKEN = 'TBD'
+env.AWS_SECRET_ACCESS_KEY_TOKEN = 'TBD'
+
+// script env vars
+env.vars = [ 'AWS_REGION=us-east-1' ]
---
+++ scripts/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd ../Pre-Elk
+terraform init
+terraform apply -var-file=../parameters/pre-elk-param.tfvars -auto-approve
+
+cd ../IAM
+terraform init
+terraform apply -auto-approve
+
+cd ../packer
+packer build -only=amazon-ebs -var-file=variables.json elasticsearch6-node.packer.json || true
+packer build -only=amazon-ebs -var-file=variables.json kibana6-node.packer.json || true
+
+cd ../terraform-aws
+vpcid=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name']|[?Value=='cpv-vpc']].VpcId" --region us-east-2 --output text)
+terraform init
+terraform plan -var 'vpc_id=${vpcid}' -var-file=../parameters/es-cluster-param.tfvars
+terraform apply -var 'vpc_id=${vpcid}' -var-file=../parameters/es-cluster-param.tfvars -auto-approve
+terraform output > /var/lib/jenkins/pipeline-output.txt
+terraform destroy -var 'vpc_id=${vpcid}' -var-file=../parameters/es-cluster-param.tfvars -auto-approve
+
+cd ../IAM
+terraform destroy -auto-approve
+
+cd ../Pre-Elk
+terraform desroy -var-file=../parameters/pre-elk-param.tfvars -auto-approve
