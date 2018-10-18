b4f666c60ede0fa91d25b1e7a181534e9033d274
shiv-rajawat <bindassmangal@gmail.com>
2018-10-18 17:07:39 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,10 @@ packer build -only=amazon-ebs -var-file=variables.json elasticsearch6-node.packe
 packer build -only=amazon-ebs -var-file=variables.json kibana6-node.packer.json || true
 
 cd ../terraform-aws
-vpcid=`aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name']|[?Value=='cpv-vpc']].VpcId" --region us-east-2 --output text`
+vpcid=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name']|[?Value=='myvpc']].VpcId" --region us-east-2 --output text)
+echo ${vpcid}
+len=${#vpcid}
+echo $len
 terraform init -backend-config='region=us-east-2'
 terraform plan -var 'vpc_id=${vpcid}' -var-file=../parameters/es-cluster-param.tfvars
 terraform apply -var 'vpc_id=${vpcid}' -var-file=../parameters/es-cluster-param.tfvars -auto-approve
@@ -23,7 +26,7 @@ terraform output > /var/lib/jenkins/pipeline-output.txt
 terraform destroy -var 'vpc_id=${vpcid}' -var-file=../parameters/es-cluster-param.tfvars -auto-approve
 
 cd ../IAM
-terraform destroy -auto-approve
+terraform destroy -var-file=../parameters/iam-param.tfvars -auto-approve
 
-cd ../Pre-Elk
+cd ../Pre-ELK
 terraform desroy -var-file=../parameters/pre-elk-param.tfvars -auto-approve
