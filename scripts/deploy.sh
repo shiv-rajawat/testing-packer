b35c90985fe71b481ce38dadf094b9abfbd9c294
shiv-rajawat <bindassmangal@gmail.com>
2018-10-18 18:19:48 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+cd $PWD/terraform-aws
+vpcid=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name']|[?Value=='cpv-vpc']].VpcId" --region us-east-2 --output text)
+terraform init -backend-config='region=us-east-2'
+#terraform plan -var "vpc_id=$vpcid" -var-file=../parameters/es-cluster-param.tfvars
+terraform apply -var "vpc_id=$vpcid" -var-file=../parameters/es-cluster-param.tfvars -auto-approve
+terraform output > /var/lib/jenkins/pipeline-output.txt
+
+
+
+#### Destroying everything
+
+terraform destroy -var "vpc_id=$vpcid" -var-file=../parameters/es-cluster-param.tfvars -auto-approve
+
+cd ../IAM
+terraform destroy -var-file=../parameters/iam-param.tfvars -auto-approve
+
+cd ../Pre-ELK
+terraform destroy -var-file=../parameters/pre-elk-param.tfvars -auto-approve
