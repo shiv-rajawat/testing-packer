656bea39831784fad99c9f6d70609888a5b3e8cf
shiv-rajawat <bindassmangal@gmail.com>
2018-10-18 18:19:09 +0530

    Update pipeline-vars.groovy

--- pipeline-vars.groovy
+++ pipeline-vars.groovy
@@ -1,8 +1,8 @@
 // the stages this pipeline supports
 PIPELINE_STEPS = [
     'Build': 'bash ./scripts/build.sh',
-    'Validate': './scripts/validate.sh',
-    'Deploy': './scripts/deploy.sh'
+    'Validate': 'bash ./scripts/validate.sh',
+    'Deploy': 'bash ./scripts/deploy.sh'
         ]
 
 // Credential Tokens
@@ -10,5 +10,6 @@ env.AWS_CREDENTIAL_ID_TOKEN = 'TBD'
 env.AWS_ACCESS_KEY_ID_TOKEN = 'TBD'
 env.AWS_SECRET_ACCESS_KEY_TOKEN = 'TBD'
 
-// script env vars
-env.vars = [ 'AWS_REGION=us-east-1' ]
+// script vars
+env.AWS_REGION = 'us-east-2'
+env.ELK_VPC_NAME = 'cpv-vpc'
