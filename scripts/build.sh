5ea5864588ba55e3e6d66048eea23483074fc9ff
shiv-rajawat <bindassmangal@gmail.com>
2018-10-18 19:04:08 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 cd $PWD/Pre-ELK
-echo "${env.AWS_REGION}"
-echo ${env.AWS_REGION}
-echo $env.AWS_REGION
-echo $AWS_REGION
-echo ${AWS_REGION}
-terraform init -backend-config='region="${env.AWS_REGION}"'
+terraform init -backend-config='region="${AWS_REGION}"'
 terraform apply -var-file=../parameters/pre-elk-param.tfvars -auto-approve
 
 
 cd ../IAM
-terraform init -backend-config='region="${env.AWS_REGION}"'
+terraform init -backend-config='region="${AWS_REGION}"'
 terraform apply -var-file=../parameters/iam-param.tfvars -auto-approve
 
 
