2d04304d661fd292d34c3e04cc27fad1f8494b78
shiv-rajawat <bindassmangal@gmail.com>
2018-11-15 16:39:30 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,11 @@
 
 cd $PWD/terraform-aws
 vpcname=${ELK_VPC_NAME}
+echo "$vpcname"
 vpcid=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='Name']|[?Value=='$vpcname']].VpcId" --region ${AWS_REGION} --output text)
+size=${#vpcid}
+echo "$size"
+echo "$vpcid"
 terraform init -backend-config="region=${AWS_REGION}"
 #terraform plan -var "vpc_id=$vpcid" -var-file=../parameters/es-cluster-param.tfvars
 terraform apply -var "access_key=$AWS_ACCESS_KEY_ID" -var "secret_key=$AWS_SECRET_ACCESS_KEY" -var "vpc_id=$vpcid" -var-file=../parameters/es-cluster-param.tfvars -auto-approve
