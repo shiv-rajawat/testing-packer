8eaa35b4ed45f2fb50a344f88cf5204001ed4bcb
shiv-rajawat <bindassmangal@gmail.com>
2018-11-15 17:50:56 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 aws s3 ls
+terraform version
 cd $PWD/Pre-ELK
 terraform init -backend-config="region=${AWS_REGION}"
 terraform apply -var "access_key=$AWS_ACCESS_KEY_ID" -var "secret_key=$AWS_SECRET_ACCESS_KEY" -var-file=../parameters/pre-elk-param.tfvars -auto-approve
